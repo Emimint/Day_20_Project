405ec200a9ea8aacbca11bda02fcca6538341470
Emimint <emilie.echevin@gmail.com>
2022-03-03 15:48:14 -0500

    add directions in snake.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from turtle import Screen, Turtle
 import time
-import Snake
+from snake import Snake
 
 screen = Screen()
-
 screen.setup(600, 600)
 screen.bgcolor("black")
 screen.title("Snake game")
@@ -11,10 +10,17 @@ screen.tracer(0)
 
 snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 game_is_On = True
 
 while game_is_On:
     screen.update()
-    time.sleep(0.5)
+    time.sleep(0.1)
+    snake.move()
 
 screen.exitonclick()
