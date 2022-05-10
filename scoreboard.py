dc75ef7465ab90e9c054cbb3ecd0d269c67d6f24
Emimint <emilie.echevin@gmail.com>
2022-05-10 18:29:07 -0400

    Add dynamic score feature
    High score is now fetched from an external file.

--- scoreboard.py
+++ scoreboard.py
@@ -4,7 +4,8 @@ FONT = ('Arial', 12, 'bold')
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.high_score = 0
+        with open("score_data.txt") as file:
+            self.high_score = int(file.read())
         self.score = 0
         self.color("white")
         self.penup()
@@ -19,8 +20,8 @@ class Scoreboard(Turtle):
     def game_over(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("score_data.txt","w") as file:
+                file.write(str(self.high_score))
         self.refresh()
         self.score = 0
         self.refresh()
-        # self.goto(0,0)
-        # self.write("GAME OVER.", False, align=ALIGNMENT, font=FONT)
